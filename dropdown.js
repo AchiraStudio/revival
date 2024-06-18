cc4b2e5d9ff23f088bda34ba9d4171ff06baea29
AchiraStudio <107109072+AchiraStudio@users.noreply.github.com>
2024-06-18 13:56:35 +0700

    Updated the page, added support for phone devices

---
+++ dropdown.js
@@ -0,0 +1,7 @@
+function showMenu() {
+    document.querySelector('#wide-screen .menu').classList.add('visible');
+}
+
+function hideMenu() {
+    document.querySelector('#wide-screen .menu').classList.remove('visible');
+}
